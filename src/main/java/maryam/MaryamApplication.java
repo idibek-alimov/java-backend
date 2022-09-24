dd4fa1d44da3e45bc4bd7c0107df70c792ac4152
alimov <alikcye.2001@gmail.com>
2022-09-24 11:23:53 +0300

    first commit

---
+++ src/main/java/maryam/MaryamApplication.java
@@ -0,0 +1,13 @@
+package maryam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaryamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaryamApplication.class, args);
+	}
+
+}
